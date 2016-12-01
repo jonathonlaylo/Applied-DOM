0d932ed0ecfedb7cd86c0bad99a25889ddedfb37
jonathon.laylo@gmail.com <jonathon.laylo@gmail.com>
2016-12-01 13:37:51 -1000

    added const btn11 and its green

--- js/app.js
+++ js/app.js
@@ -188,7 +188,7 @@
    *   passing 2 additional hardcoded arguments:
    *     null, 'light-green'
    */
-
+   const setLight11Green = setMyLightClass.bind(btn11, null, 'light-green');
 
   /*
    * Add a click event listener to btn11
@@ -196,7 +196,9 @@
    * the body of the event handler function
    * will invoke the setLight11Green function
    */
-
+   btn11.addEventListener('click', function(evetn){
+    setLight11Green();
+   });
 
   /*
    * Declare a new const named setLight12Class
