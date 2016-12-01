07f665f3e20dff0662dd16f8be225a26b89a9783
jonathon.laylo@gmail.com <jonathon.laylo@gmail.com>
2016-11-30 22:25:11 -1000

    btn3 turned green

--- js/app.js
+++ js/app.js
@@ -82,7 +82,7 @@
    * Add a click event listener to btn3
    * the handler method will be a reference to the setMyLightGreen function
    */
-
+   btn3.addEventListener('click', setMyLightGreen);
 
   /*
    * Add a click event listener to btn4
