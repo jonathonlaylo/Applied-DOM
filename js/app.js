0cb85da0bb7f5c33bc3914fa2cc0f3fcc5e85a6d
jonathon.laylo@gmail.com <jonathon.laylo@gmail.com>
2016-12-01 13:42:23 -1000

    btn 14 done

--- js/app.js
+++ js/app.js
@@ -244,7 +244,7 @@
    * that is the result of invoking the Function prototype method, bind
    * on the setMyLightGreen function
    */
-
+   btn14.addEventListener('click', setMyLightGreen.bind(btn14));
 
   /*
    * Add a click event listener to btn15
